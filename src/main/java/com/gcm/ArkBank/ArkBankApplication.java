07e045cd5c3e9657e64941d0aed46dcde08a76e5
Raquel Brito <raquelbrito662@gmail.com>
2025-05-16 20:41:25 -0300

    build:  setup inicial do projeto com Spring Initializr

---
+++ src/main/java/com/gcm/ArkBank/ArkBankApplication.java
@@ -0,0 +1,13 @@
+package com.gcm.ArkBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBankApplication.class, args);
+	}
+
+}
