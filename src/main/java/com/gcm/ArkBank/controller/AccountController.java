106fe31c7eef6c907d9893ac945afe4b19de9711
Raquel Brito <raquelbrito662@gmail.com>
2025-05-17 11:12:53 -0300

    feat: implementacao do scanner

--- src/main/java/com/gcm/ArkBank/controller/AccountController.java
+++ src/main/java/com/gcm/ArkBank/controller/AccountController.java
@@ -1,17 +1,22 @@
 package com.gcm.ArkBank.controller;
 
 import com.gcm.ArkBank.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
 @Component
-public class AccountController {
+public class AccountController implements CommandLineRunner {
     private final AccountService accountService;
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
 
-    private Scanner scanner = new Scanner(System.in);
+    @Override
+    public void run(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem-vindo ao Banco!");
+    }
 }
