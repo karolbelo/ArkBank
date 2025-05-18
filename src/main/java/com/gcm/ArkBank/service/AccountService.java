2ba670167f26abf254498ee17c5e26bcf8a6c312
Raquel Brito <raquelbrito662@gmail.com>
2025-05-18 19:54:10 -0300

    feat: add metodo debitar no service

--- src/main/java/com/gcm/ArkBank/service/AccountService.java
+++ src/main/java/com/gcm/ArkBank/service/AccountService.java
@@ -30,6 +30,18 @@ public class AccountService {
         }
     }
 
+    public void debitar(int numero, double valor) {
+        Account conta = contas.get(numero);
+        if (conta == null) {
+            System.out.printf("Conta inválida.");
+            return;
+        }
+
+        conta.setBalance(conta.getBalance() - valor);
+        System.out.printf("Debito de R$ %.2f realizado com sucesso.%n", valor);
+        System.out.printf("Novo saldo: R$ %.2f%n", conta.getBalance());
+    }
+
     private boolean exist(int numero) {
         return contas.containsKey(numero);
     }
